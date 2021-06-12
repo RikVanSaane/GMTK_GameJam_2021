695182c8de44044c2fd69c026cdcdef3762acedd
Rik van Saane <72133509+TheRedKing1337@users.noreply.github.com>
2021-06-12 15:40:52 +0200

    new push (Dont ask me what I did)

--- Assets/Scripts/FinishPoint.cs
+++ Assets/Scripts/FinishPoint.cs
@@ -5,21 +5,30 @@ using UnityEngine;
 public class FinishPoint : MonoBehaviour
 {
     private bool isStartupFade = true;
+    private bool isReloadFade;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Bear"))
         {
-            //TODO Screenwipe
             GameManager.Instance.currentLevel++;
             GetComponent<Animator>().Play("FadeOut");
+            isStartupFade = false;
         }
+    }
+    public void ReloadLevel()
+    {
+        isReloadFade = true;
+        GetComponent<Animator>().Play("FadeOut");
     }
     private void LoadNextLevel()
     {
+        if (isReloadFade)
+        {
+            GameManager.Instance.LoadNextLevel();
+        }
         if (isStartupFade)
         {
-            isStartupFade = false;
             return;
         }
         GameManager.Instance.LoadNextLevel();
